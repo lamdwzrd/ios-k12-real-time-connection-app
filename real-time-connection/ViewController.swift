7a33d8f3c07ce56365d92bdbaea0aa2de3cf5697
lamdwzrd <lamdwzrd@gmail.com>
2023-10-07 00:49:24 +0700

    completed

--- real-time-connection/ViewController.swift
+++ real-time-connection/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let URL_SERVER_01 = "http://localhost:3000/check"
+    let URL_SERVER_02 = "http://localhost:3001/check"
+
+    @IBOutlet var img_server01: UIImageView!
+    @IBOutlet var img_server02: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [self] Timer in
+            checkServer(domain: URL_SERVER_01, img: img_server01)
+            checkServer(domain: URL_SERVER_02, img: img_server02)
+        }
     }
 
+    func checkServer(domain: String, img: UIImageView) {
+        let url = URL(string: domain)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
 
+        let _ = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+
+            if error == nil {
+                DispatchQueue.main.async {
+                    img.image = UIImage(named: "online")
+                }
+            } else {
+                DispatchQueue.main.async {
+                    img.image = UIImage(named: "offline")
+                }
+            }
+        }.resume()
+    }
 }
-
